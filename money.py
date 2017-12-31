80b6503720e2910b8f343ce79e5e7ba60c3ffc14
Mitchell Woodbine <mitchellwoodbine@gmail.com>
2017-12-31 15:36:16 +0000

    Fixed Money Class
    This class was causing huge problems originally, glad I finally
    spent the time to fix it.

--- money.py
+++ money.py
@@ -3,34 +3,44 @@ class Money:
     def __init__(self, amount=0, accuracy=8):
         self.amount = 0
         self.accuracy = int(accuracy)
+        self.set(amount)
 
-        self.set_amount(amount)
+    def set(self, x):
+        value = float(str(x))
+        self.amount = int(value*(10**self.accuracy))
 
-    def set_amount(self, x):
-        self.amount = int(float(x)*(10**self.accuracy))
+    def get_accuracy(self,x):
+        if type(x) is Money:
+            if self.accuracy > x.accuracy:
+                return self.accuracy
+            else:
+                return x.accuracy
+        else:
+            return self.accuracy
 
-#    def __add__(self, x):
-#        if self.accuracy > x.accuracy:
-#            m = Money(accuracy=self.accuracy)
-#        else:
-#            m = Money(accuracy=x.accuracy)
-#        m.amount = int(self.amount + x.amount)
-#        return m
-#
-#    def __sub__(self, x):
-#        m = Money()
-#        m.amount = int(self.amount - x.amount)
-#        return m
-#
-#    def __mul__(self, x):
-#        m = Money()
-#        m.amount = int(self.amount * x.amount)
-#        return m
-#
-#    def __div__(self, x):
-#        m = Money()
-#        m.amount = int(self.amount / x.amount * (10**self.accuracy))
-#        return m
+    def __add__(self, x):
+        accuracy = self.get_accuracy(x)
+        a = self.float()
+        x = float(str(x))
+        return Money(a+x,accuracy)
+
+    def __sub__(self, x):
+        accuracy = self.get_accuracy(x)
+        a = self.float()
+        x = float(str(x))
+        return Money(a-x,accuracy)
+
+    def __mul__(self, x):
+        accuracy = self.get_accuracy(x)
+        a = self.float()
+        x = float(str(x))
+        return Money(a*x,accuracy)
+
+    def __div__(self, x):
+        accuracy = self.get_accuracy(x)
+        a = self.float()
+        x = float(str(x))
+        return Money(a/x,accuracy)
 
     def __gt__(self, x):
         return self.amount > x.amount
@@ -50,6 +60,24 @@ class Money:
     def __eq__(self, x):
         return self.amount == x.amount
 
+    def __float__(self):
+        return float(self.amount)/(10**self.accuracy)
+
+    def float(self):
+        return self.__float__()
+
     def __str__(self):
-        result = float(float(self.amount) / (10**self.accuracy))
-        return "{:.8f}".format(result)
+        result = float(self.amount) / (10**self.accuracy)
+        accuracy = "{:." + str(self.accuracy) + "f}"
+        return accuracy.format(result)
+
+    def str(self):
+        return self.__str__()
+
+
+# if __name__ == "__main__":
+#     a = Money(5)
+#     b = Money(3)
+#     c = (((a-(a*b))+b)*(a/b))
+#     print(c)
+# 
